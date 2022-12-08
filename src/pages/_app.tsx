5cb6996c64d37b00e7246006b848cf998f150b1f
Lília Paula <liliaxtz2@gmail.com>
2022-12-08 01:11:41 -0300

    wip: general settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import { client, ssrCache } from "../lib/urql";
+
+import type { AppProps } from "next/app";
+import { Provider } from "urql";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //verificação para evitar refazer query no client caso já tenha feito no sssr / reaproveitando o cache do ssr
+  if (pageProps.urqState) {
+    ssrCache.restoreData(pageProps.urqlState);
+  }
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
